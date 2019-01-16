b4244c1c197afef362ac245dcddbb2642b54857c
George Kaplan <george.kaplan@visionistinc.com>
2019-01-16 18:30:12 -0500

    move formatting and dir walking into custom methods

--- src/bin/trx/main.rs
+++ src/bin/trx/main.rs
@@ -1,28 +1,29 @@
-use std::ffi::OsStr;
-use std::fs::FileType;
-use std::path::{Path, PathBuf};
+use std::io::Result;
+use std::path::PathBuf;
 
-use ignore::overrides::{Override, OverrideBuilder};
-use ignore::WalkBuilder;
 use structopt::StructOpt;
+
+use trx::*;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "trx", about = "A tree command that gets it")]
-struct Cli {
+struct Config {
     #[structopt(short)]
     all: bool,
     #[structopt(short)]
     directories: bool,
     #[structopt(short = "l")]
     symlinks: bool,
-    #[structopt(short)]
-    full_paths: bool,
     #[structopt(short = "x")]
     stay_on_fs: bool,
     #[structopt(short = "L")]
     max_depth: Option<usize>,
+    #[structopt(short)]
+    full_paths: bool,
     #[structopt(short = "Q")]
     quote_names: bool,
+    #[structopt(short = "i")]
+    no_indent: bool,
     #[structopt(short = "P")]
     keep_pattern: Vec<String>,
     #[structopt(short = "I")]
@@ -31,82 +32,19 @@ struct Cli {
     dir: Option<PathBuf>,
 }
 
-fn build_override(config: &Cli, root: &Path) -> Override {
-    let mut over = OverrideBuilder::new(root);
-
-    if !config.keep_pattern.is_empty() {
-        over.add("!*").unwrap();
-    }
-
-    for glob in config.keep_pattern.iter() {
-        over.add(&glob).unwrap();
-    }
-
-    for glob in config.ignore_pattern.iter() {
-        over.add(&format!("!{}", glob)).unwrap();
-    }
-
-    over.build().unwrap()
-}
-
-fn format_entry(entry: &Path, root: &Path, config: &Cli) -> String {
-    let mut new = PathBuf::from(".");
-
-    let name = if config.full_paths && entry != root {
-        let tmp = entry
-            .canonicalize()
-            .unwrap()
-            .strip_prefix(&root.canonicalize().unwrap())
-            .unwrap()
-            .to_path_buf();
-        new.push(tmp);
-        new.to_str().unwrap()
-    } else {
-        entry
-            .file_name()
-            .unwrap_or_else(|| OsStr::new("."))
-            .to_str()
-            .unwrap()
-    };
-
-    if config.quote_names {
-        format!("\"{}\"", name)
-    } else {
-        name.to_string()
-    }
-}
-
-fn main() {
-    let cfg = Cli::from_args();
+fn main() -> Result<()> {
+    let cfg = Config::from_args();
     let current_dir = PathBuf::from(".");
     let dir = cfg.dir.as_ref().unwrap_or(&current_dir);
+    let mut tree = Dir::from(dir).with_format(FormatOpts {
+        full_paths: cfg.full_paths,
+        indent: !cfg.no_indent,
+        quote_names: cfg.quote_names,
+    });
 
-    // find the files
-    for entry in WalkBuilder::new(&dir)
-        .max_depth(cfg.max_depth)
-        .hidden(!cfg.all)
-        .follow_links(cfg.symlinks)
-        .overrides(build_override(&cfg, &dir))
-        .build()
-    {
-        match entry {
-            Ok(e) => {
-                if cfg.directories
-                    && e.file_type()
-                        .as_ref()
-                        .map(FileType::is_file)
-                        .unwrap_or(false)
-                {
-                    continue;
-                }
+    tree.sort_children();
 
-                // format it
-                let name = format_entry(e.path(), &dir, &cfg);
+    println!("{}", tree);
 
-                // print it
-                println!("{}", name);
-            }
-            Err(e) => eprintln!("ERROR: {}", e),
-        }
-    }
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,115 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::ffi::OsStr;
+use std::fmt;
+use std::fs;
+use std::path::PathBuf;
+
+const SUPER_DIR: char = '│';
+const PARENT_NTH: char = '├';
+const PARENT_LAST: char = '└';
+const INDENT: &str = "── ";
+
+#[derive(Clone)]
+pub struct Dir {
+    path: PathBuf,
+    contents: Vec<Dir>,
+    nest: Vec<bool>,
+    format: FormatOpts,
+}
+
+impl Dir {
+    fn with_nest_level(self, nest: Vec<bool>) -> Self {
+        Self { nest, ..self }
     }
+
+    pub fn with_format(self, format: FormatOpts) -> Self {
+        Self { format, ..self }
+    }
+
+    fn format_name(&self) -> String {
+        let stringified = if self.format.full_paths {
+            self.path.to_str()
+        } else {
+            self.path.file_name().unwrap_or(OsStr::new(".")).to_str()
+        }
+        .unwrap();
+
+        if self.format.quote_names {
+            format!("\"{}\"", stringified)
+        } else {
+            stringified.to_string()
+        }
+    }
+
+    pub fn sort_children(&mut self) {
+        self.contents.sort_unstable_by_key(|v| v.path.clone());
+        self.contents.iter_mut().for_each(|c| c.sort_children());
+    }
+}
+
+impl fmt::Display for Dir {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "{}", self.format_name())?;
+
+        for (idx, member) in self.contents.iter().enumerate() {
+            let mut hanger = PARENT_NTH;
+            let mut new_depth = self.nest.clone();
+
+            if idx + 1 == self.contents.len() {
+                hanger = PARENT_LAST;
+                new_depth.push(false);
+            } else {
+                new_depth.push(true);
+            }
+
+            let adjusted_member = member
+                .to_owned()
+                .with_nest_level(new_depth)
+                .with_format(self.format);
+
+            if self.format.indent {
+                let space_before = self
+                    .nest
+                    .iter()
+                    .map(|b| format!("{:4}", if *b { SUPER_DIR } else { ' ' }))
+                    .collect::<String>();
+
+                write!(f, "{}{}{}{}", space_before, hanger, INDENT, adjusted_member)?;
+            } else {
+                write!(f, "{}", adjusted_member)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl From<&PathBuf> for Dir {
+    fn from(obj: &PathBuf) -> Self {
+        if obj.is_dir() {
+            Self {
+                path: obj.to_owned(),
+                nest: Vec::new(),
+                contents: fs::read_dir(obj)
+                    .unwrap()
+                    .into_iter()
+                    .map(|e| Self::from(&e.unwrap().path()))
+                    .collect::<Vec<_>>(),
+                format: FormatOpts::default(),
+            }
+        } else {
+            Self {
+                path: obj.to_owned(),
+                nest: Vec::new(),
+                contents: Vec::new(),
+                format: FormatOpts::default(),
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+pub struct FormatOpts {
+    pub full_paths: bool,
+    pub indent: bool,
+    pub quote_names: bool,
 }
